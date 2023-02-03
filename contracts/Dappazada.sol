8b9ebd4cf087e5eb0dc4d7a43cef95e9d6f555bf
joash-lee <91481123+joash-lee@users.noreply.github.com>
2023-02-03 16:52:58 +0700

    Added List function to Smart Contract

--- contracts/Dappazada.sol
+++ contracts/Dappazada.sol
@@ -2,11 +2,42 @@
 pragma solidity ^0.8.9;
 
 contract Dappzada {
-    string public name;
     address public owner;
-
+    struct Item {
+        uint256 id;
+        string name;
+        string category;
+        string image;
+        uint256 cost;
+        uint256 rating;
+        uint256 stock;
+    }
+    mapping (uint256=> Item) public items;
+    event List(string name, uint256 cost, uint256 quantity);
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
     constructor(){
-        name = "Dappzada";
         owner = msg.sender;
+    }
+
+    function list(
+        uint256 _id,
+        string memory _name,
+        string memory _category,
+        string memory _image,
+        uint256 _cost,
+        uint256 _rating,
+        uint256 _stock) public onlyOwner{
+            Item memory item = Item(_id,
+            _name,
+            _category,
+            _image,
+            _cost,
+            _rating,
+            _stock);
+            items[_id] = item;
+            emit List(_name, _cost, _stock);
     }
 }
--- test/Dappzada.js
+++ test/Dappzada.js
@@ -4,6 +4,14 @@ const { ethers } = require("hardhat");
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), "ether");
 };
+const ID = 1;
+const NAME = "Shoes";
+const CATEGORY = "Clothing";
+const IMAGE =
+  "https://ipfs.io/ipfs/QmTYEboq8raiBs7GTUg2yLXB3PMz6HuBNgNfSZBx5Msztg/shoes.jpg";
+const COST = tokens(1);
+const RATING = 4;
+const STOCK = 5;
 
 describe("Dappzada", () => {
   let dappzada;
@@ -13,13 +21,31 @@ describe("Dappzada", () => {
     dappzada = await Dappzada.deploy();
   });
   describe("Deployment", () => {
-    it("Sets the owner", async () => {
+    it("sets the owner", async () => {
       const owner = await dappzada.owner();
       expect(owner).to.equal(deployer.address);
     });
-    it("has a name", async () => {
-      const name = await dappzada.name();
-      expect(name).to.equal("Dappzada");
+  });
+  describe("Listing", () => {
+    let transaction;
+    beforeEach(async () => {
+      transaction = await dappzada
+        .connect(deployer)
+        .list(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
+      await transaction.wait();
+    });
+    it("returns the item attributes", async () => {
+      const item = await dappzada.items(ID);
+      expect(item.id).to.equal(ID);
+      expect(item.name).to.equal(NAME);
+      expect(item.category).to.equal(CATEGORY);
+      expect(item.image).to.equal(IMAGE);
+      expect(item.cost).to.equal(COST);
+      expect(item.rating).to.equal(RATING);
+      expect(item.stock).to.equal(STOCK);
+    });
+    it("emits List event", () => {
+      expect(transaction).to.emit(dappzada, "List");
     });
   });
 });
