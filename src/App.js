d91234ad8b352e956e31a906885e4dd5a920ecb3
joash-lee <91481123+joash-lee@users.noreply.github.com>
2023-02-17 00:22:18 +0700

    Completed Website: Needs Updating Descriptions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
 
 // Components
@@ -13,9 +13,78 @@ import Dappzada from "./abis/Dappzada.json";
 import config from "./config.json";
 
 function App() {
+  const [account, setAccount] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [dappzada, setDappzada] = useState(null);
+
+  const [electronics, setElectronics] = useState(null);
+  const [clothing, setClothing] = useState(null);
+  const [toys, setToys] = useState(null);
+  const [item, setItem] = useState({});
+  const [toggle, setToggle] = useState(false);
+
+  const togglePop = (item) => {
+    setItem(item);
+    toggle ? setToggle(false) : setToggle(true);
+  };
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
+    const network = await provider.getNetwork();
+
+    const dappzada = new ethers.Contract(
+      config[network.chainId].dappzada.address,
+      Dappzada,
+      provider
+    );
+    setDappzada(dappzada);
+    const items = [];
+    for (var i = 0; i < 9; i++) {
+      const item = await dappzada.items(i + 1);
+      items.push(item);
+    }
+    const electronics = items.filter((item) => item.category === "electronics");
+    const clothing = items.filter((item) => item.category === "clothing");
+    const toys = items.filter((item) => item.category === "toys");
+
+    setElectronics(electronics);
+    setClothing(clothing);
+    setToys(toys);
+  };
+
+  useEffect(() => {
+    loadBlockchainData();
+  }, []);
+
   return (
     <div>
-      <h2>Welcome to Dappzada</h2>
+      <Navigation account={account} setAccount={setAccount} />
+      <h2>Dappzada Store</h2>
+      {electronics && clothing && toys && (
+        <>
+          <Section
+            title={"Clothing & Jewelry"}
+            items={clothing}
+            togglePop={togglePop}
+          />
+          <Section
+            title={"Electronics & Gadgets"}
+            items={electronics}
+            togglePop={togglePop}
+          />
+          <Section title={"Toys & Gaming"} items={toys} togglePop={togglePop} />
+        </>
+      )}
+      {toggle && (
+        <Product
+          item={item}
+          provider={provider}
+          account={account}
+          dappzada={dappzada}
+          togglePop={togglePop}
+        />
+      )}
     </div>
   );
 }
